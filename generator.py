39b2b9e013cbdd492b6059a6e49a773fe2fd90ff
Patrik41089 <rad41089@spseol.cz>
2023-12-09 14:05:15 +0100

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,2 +1,15 @@
-#gg ted napsat generator a dostat zase za 4
-a =2
+import random
+seznam = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","w","x","y","z"]
+samohlasky = ["a","e","i","y","o","u"]
+anglictina_predlozky = ["an","the", "in", "on", "at", "up"]
+anglictina_konce = ["ing","tion"]
+
+def nahodne_slovo(delka):
+    slovo = ''.join(random.choice(seznam) for i in range(delka))
+    return slovo
+
+vygenerovana_slova = ' '.join(nahodne_slovo(random.randint(2, 10)) for i in range(random.randint(10,20)))
+
+
+
+print(vygenerovana_slova)
