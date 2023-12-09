69b86a1285c90cb99aa5a0643c9926ad278127b6
Patrik41089 <rad41089@spseol.cz>
2023-12-09 15:38:48 +0100

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,19 +1,46 @@
 import random
 seznam = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","w","x","y","z"]
-samohlasky = ["a","e","i","y","o","u"]
+
+#samohlasky = ["a","e","i","y","o","u"]
+
 anglictina_predlozky = ["an","the", "in", "on", "at", "up"]
-anglictina_konce = ["ing","tion"]
 
-def nahodne_slovo(delka):
+anglictina_konce = ["ing","tion","ed","s"]
+
+#generace písmena s délkou k pouziti na slozeni slova z pismen
+def nahodne_slovo(delka):               
     slovo = ''.join(random.choice(seznam) for i in range(delka))
-    return slovo
+    return slovo    #vraceni hodnoty z funkce abych ji mohl pouzit
 
+#generace koncovky
 def nahodna_koncovka():
-    koncovka =  ''.join(random.choice(anglictina_konce))
+    koncovka = random.choice(anglictina_konce)
     return koncovka
 
-vygenerovana_slova = ' '.join(nahodne_slovo(random.randint(2, 10)) for i in range(random.randint(2,10)))
+#generace predlozky
+def nahodna_predlozka():
+    predlozka = random.choice(anglictina_predlozky)
+    return predlozka
 
-text = ''.join(vygenerovana_slova + random.choice(anglictina_konce))
+#generace textu, z písmena slovo s koncovkou
+def generovani_text(pocet_slov): #pocet slov k pouziti ke generovani nahodneho mnozstvi textu pozdeji
+    vygenerovana_slova = [] #prazdny seznam kam se mi budou vypisovat generovana slova s koncovkou
+    predlozka_predchozi = False
+    for i in range(pocet_slov):
+        slovo = nahodne_slovo(random.randint(2, 10))
+        if random.randint(0,1) < 0.5:  #0.5 je polovina rozsahu generace nahodneho cisla od 0 do 1 takze je to 50% sance na pridani koncovky ke kazdemu generovanemu slovu
+            slovo += nahodna_koncovka() #slouceni slova a koncovky
+            
+        if not predlozka_predchozi and random.randint(0,1) < 0.8:
+            vygenerovana_slova.append(nahodna_predlozka())
+            predlozka_predchozi = True
+        else:
+            predlozka_predchozi = False
+        
+        vygenerovana_slova.append(slovo) #vlozeni hotoveho slova s koncovku do seznamu ktery vypisi
 
+    text = ' '.join(vygenerovana_slova) #slouceni pomoci join aby se to nevypisovalo v seznamu
+    return text
+
+text = generovani_text(random.randint(20, 150))
 print(text)
